a6ddfba50b0e1ea3e55f6c18b1e4a4d6d4a0732b
Poojitha3007 <poojitha.ks2024@vitstudent.ac.in>
2026-07-18 11:06:10 +0000

    Updated appointment.py in main branch

--- appointment.py
+++ appointment.py
@@ -1,5 +1,9 @@
 print("hospital appointment booking system")
+
 def register_patient(name):
     print(f"Patient {name} registered successfully.")
+
 def login_user(username, password):
     print(f"User {username} logged in.")
+
+print("Appointment booked from Main Branch")
